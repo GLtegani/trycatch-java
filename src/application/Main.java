ba4e7debc1c532b5749dbdcbbba440ae1adf6683
Gabriel <gabrielbtegani@gmail.com>
2024-07-25 13:16:27 -0300

    learning about try catch and how I can make my exceptions for program

---
+++ src/application/Main.java
@@ -0,0 +1,44 @@
+package application;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Room number: ");
+            Integer roomNumber = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            LocalDate checkIn = LocalDate.parse(sc.nextLine(), formatter);
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            LocalDate checkOut = LocalDate.parse(sc.nextLine(), formatter);
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.print(reservation);
+            System.out.println();
+
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = LocalDate.parse(sc.nextLine(), formatter);
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = LocalDate.parse(sc.nextLine(), formatter);
+            reservation.updateDates(checkIn, checkOut);
+            System.out.print(reservation);
+            sc.close();
+        } catch (DomainException error) {
+            System.out.println("Error in reservation: " + error.getMessage());
+        } catch (Exception error) {
+            System.out.println("Unexpected error");
+        }
+
+    }
+}
---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,64 @@
+package model.entities;
+
+import model.exceptions.DomainException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) throws DomainException {
+        if(!checkOut.isAfter(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public final Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public final void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public final LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public final LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public final int duration() {
+        return this.checkOut.compareTo(this.checkIn);
+    }
+
+    public final void updateDates(LocalDate checkIn, LocalDate checkOut) throws DomainException {
+        LocalDate now = LocalDate.now();
+
+        if(checkIn.isBefore(now) || checkOut.isBefore(now)) {
+            throw new DomainException("Reservation dates for update must be future dates");
+        } else if(checkOut.isBefore(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        } else {
+            this.checkIn = checkIn;
+            this.checkOut = checkOut;
+        }
+    }
+
+    @Override
+    public final String toString() {
+        return "Reservation: Room "
+                + this.roomNumber
+                + ", check-in: " + this.formatter.format(this.checkIn)
+                + ", check-out: " + this.formatter.format(this.checkOut)
+                + ", " + this.duration() + " nights";
+    }
+}
